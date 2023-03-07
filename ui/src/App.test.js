2b45c228fab34cf681ddd7682a325c6a217ec9b7
Jody <113991637+Jody-Anne@users.noreply.github.com>
2023-03-07 13:06:23 +0200

    update

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,8 +1,42 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import MyComponent from './MyComponent';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('MyComponent', () => {
+  it('should render search results and favourites list correctly', () => {
+    // Mock the axios requests
+    jest.mock('axios', () => ({
+      get: jest.fn(() => Promise.resolve({ data: [] })),
+      post: jest.fn(() => Promise.resolve({ data: [] })),
+      delete: jest.fn(() => Promise.resolve({ data: [] })),
+    }));
+
+    // Render the component
+    render(<MyComponent />);
+
+    // Test the search form
+    const searchInput = screen.getByRole('textbox', { name: /search/i });
+    const searchButton = screen.getByRole('button', { name: /search/i });
+    expect(searchInput).toBeInTheDocument();
+    expect(searchButton).toBeInTheDocument();
+
+    // Test the media type select
+    const mediaSelect = screen.getByRole('combobox', { name: /media/i });
+    expect(mediaSelect).toBeInTheDocument();
+
+    // Test the search results
+    const searchResultsList = screen.getByRole('list', { name: /search results/i });
+    expect(searchResultsList).toBeInTheDocument();
+
+    // Test the favourites list
+    const favouritesList = screen.getByRole('list', { name: /favourites/i });
+    expect(favouritesList).toBeInTheDocument();
+
+    // Test the favourite and delete buttons
+    const favouriteButtons = screen.getAllByRole('button', { name: /favourite/i });
+    const deleteButtons = screen.getAllByRole('button', { name: /delete/i });
+    expect(favouriteButtons.length).toBe(0);
+    expect(deleteButtons.length).toBe(0);
+  });
 });
